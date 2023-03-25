d2792457bce57822c85cfa55d60206179b0866e9
Alexander Tretyackov <tretyackov.aleks2012@yandex.ru>
2023-03-26 00:25:03 +0500

    feat(frontend): Разметка для просмотра выбранного объекта

--- src/ClientApp/accounter-qr-frontend/src/App.js
+++ src/ClientApp/accounter-qr-frontend/src/App.js
@@ -1,16 +1,50 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+var thing = JSON.parse(
+  '{ "name": "Стол", "type": "Мебель", "creationDateTime": "2028-10-31T17:04:32.0000000", "attributes": {"Материал": "Дерево", "Ножки" : 4} }'
+);
+console.log(thing);
+
 function ThingsList() {
-  return <p>Список вещей</p>;
+  return (
+    <div className="frame">
+      <h3>Список вещей</h3>
+    </div>
+  );
 }
 
 function NewThing() {
-  return <p>Добавление новой вещи</p>;
+  return (
+    <div className="frame">
+      <h3>Добавление новой вещи</h3>
+    </div>
+  );
+}
+
+export function GetThingAttributes(thing) {
+  var keys = Object.keys(thing.attributes);
+  return (
+    <ul>
+      {keys.map((key) => (
+        <li key={key}>
+          {key}: {thing.attributes[key]}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 function ThingPreview() {
-  return <p>Отображение информации о выбранной вещи</p>;
+  return (
+    <div className="frame">
+      <h3>Просмотр выбранного объекта</h3>
+      <p>Имя: {thing.name}</p>
+      <p>Тип: {thing.type}</p>
+      <p>Дата добавления: {thing.creationDateTime}</p>
+      {GetThingAttributes(thing)}
+    </div>
+  );
 }
 
 function App() {
@@ -19,8 +53,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <NewThing />
-        <ThingPreview />
-        <ThingsList />
+        <div className="test">
+          <ThingsList />
+          <ThingPreview />
+        </div>
       </header>
     </div>
   );
