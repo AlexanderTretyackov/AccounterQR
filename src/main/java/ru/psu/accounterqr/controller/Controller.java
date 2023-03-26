ebb22b9fa01aadda4d84579ca21273a8c776ba00
ns.lashov <niccolum129@gmail.com>
2023-03-26 22:18:30 +0500

    подготовка для слияния с main

--- src/main/java/ru/psu/accounterqr/controller/Controller.java
+++ src/main/java/ru/psu/accounterqr/controller/Controller.java
@@ -13,6 +13,7 @@ import ru.psu.accounterqr.repository.ObjectRepository;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,19 +24,25 @@ public class Controller {
     @Autowired
     ObjectRepository objectRepository;
 
+    private final static String dateTimeFormat = "dd-MM-yyyy-[HH]-[mm]";
+
     @PostMapping("/get-object")
     public ObjectEntity getObjectById(@RequestParam String id){
         return objectRepository.getObjectEntitiesById(id);
-
     }
 
     @PostMapping ("/add-object")
     public void addObject(@RequestBody ObjectEntity object) throws WriterException, IOException {
         object.setCreationDateTime(LocalDateTime.now());
 
+        String qrcodeName = String.format("%s_%s_%s",
+                object.getName(),
+                object.getType(),
+                object.getCreationDateTime().format(DateTimeFormatter.ofPattern(dateTimeFormat)));
+
         QrGenerator qrGenerator = new QrGenerator();
         BufferedImage bufferedImage = qrGenerator.generate(object);
-        qrGenerator.saveAsPicture(bufferedImage, ".\\", "test", "png");
+        qrGenerator.saveAsPicture(bufferedImage, ".\\qrcodes\\", qrcodeName, "png");
         objectRepository.save(object);
     }
 
@@ -43,11 +50,4 @@ public class Controller {
     public List<ObjectEntity> objectEntities (){
        return objectRepository.findAll();
     }
-
-
-
-
-
-
-
 }
