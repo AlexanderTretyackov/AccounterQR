a085654e89133b26f785bbd4cc350a19a2fda749
Alexander Tretyackov <tretyackov.aleks2012@yandex.ru>
2023-04-02 12:50:19 +0500

    feat: Условная инициализация более осмысленного примера

--- src/main/java/ru/psu/accounterqr/App.java
+++ src/main/java/ru/psu/accounterqr/App.java
@@ -11,31 +11,39 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class App {
 
     @Autowired
     private ObjectRepository objectRepository;
-
+    private boolean requiredInitDB = false;
     public static void main(String[] args) throws IOException {
         Files.createDirectories(Paths.get(".\\qrcodes"));
-
         SpringApplication.run(App.class, args);
 
     }
 
     @PostConstruct
     public void initDB() {
-
+        if(!requiredInitDB)
+        {
+            return;
+        }
         List<ObjectEntity> entities = Arrays.asList(
-                new ObjectEntity("id1", "test1", "type1"),
-                new ObjectEntity("id2", "test2", "type9"),
-                new ObjectEntity("id3", "test3", "type5")
-        );
-
+                new ObjectEntity("id1", "Стол1", "Стол",
+                        new LinkedHashMap<String, String>() {{
+                            put("Число ножек", "4");
+                            put("Цвет", "Белый");
+                        }}),
+                new ObjectEntity("id2", "Стол2", "Стол",
+                        new LinkedHashMap<String, String>() {{
+                            put("Число ножек", "1");
+                            put("Цвет", "Черный");
+                        }}));
         objectRepository.saveAll(entities);
-
     }
 }
